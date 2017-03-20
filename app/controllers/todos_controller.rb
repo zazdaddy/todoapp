b310bb7f70f8679bc2b0e32ec207d9509ffa8d7f
zazdaddy <toddbmooret@gmail.com>
2017-03-20 15:40:28 +0000

    Add destroy method

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -38,6 +38,13 @@ class TodosController < ApplicationController
     @todos=Todo.all
   end
   
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    flash[:notice] = "Todo was successfully destroyed"
+    redirect_to todos_path
+  end
+  
   private
   
     def todo_params
